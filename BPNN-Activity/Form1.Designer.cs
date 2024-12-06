64d7babab7e113453fa41a8a5f2e660fb3afc502
elib00 <joshuanapinas19@gmail.com>
2024-12-06 14:31:53 +0800

    [Joshua] bpnn

--- BPNN-Activity/Form1.Designer.cs
+++ BPNN-Activity/Form1.Designer.cs
@@ -52,6 +52,7 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "create BPNN";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -61,6 +62,7 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Train the Neural Net";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -70,6 +72,7 @@
             this.button3.TabIndex = 3;
             this.button3.Text = "Test";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // textBox2
             // 
--- BPNN-Activity/Form1.cs
+++ BPNN-Activity/Form1.cs
@@ -7,14 +7,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Backprop;
 
 namespace BPNN_Activity
 {
     public partial class Form1 : Form
     {
+        public NeuralNet nn;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(2, 100, 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (nn == null) return;
+            for(int x = 0; x < 100; x++)
+            {
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (nn == null) return;
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+ 
+            textBox3.Text = "" + nn.getOuputData(0);
         }
     }
 }
